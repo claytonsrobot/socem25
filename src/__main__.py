b78fb64cce5791753d768ee40419f132f7900546
Clayton Bennett <claytonrobot@gmail.com>
2025-04-06 19:57:36 -0500

    repo time

--- src/__main__.py
+++ src/__main__.py
@@ -1,13 +1,16 @@
 # __main__.py
-from shell import main
-
+from src import main
+from pprint import pprint as pprint
 def cli_entry():
     try:
         # Launch main
-        main()
+        src.main.run()()
         #shell.shell.main()
     except Exception as e:
         print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    cli_entry()
+    app = cli_entry()
+    print(f"app = {app}")
+    #print(f"app.__doc__ = {app.__doc__}")
+    pprint(app.__doc__)
